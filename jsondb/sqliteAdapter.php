52d10fbd4ca0330d7d3025ebe99cc5b4f552dcd3
승근 현 <sk@mynameismira.com>
2019-12-03 08:04:38 +0900

    updated getid3 lib

--- jsondb/sqliteAdapter.php
+++ jsondb/sqliteAdapter.php
@@ -5,13 +5,16 @@
     header('Access-Control-Max-Age: 1000');
 	header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 	header('Content-Type', 'application/json; charset=utf-8');	
+
 	$dbfile = getcwd().DIRECTORY_SEPARATOR."sqlite.db";
 	if(!file_exists($dbfile)) {
-		$db = new SQLite3($dbfile);			
+		$db = new SQLite3($dbfile);
 		$db->exec('CREATE TABLE casts (records json)');
 		$db->exec('CREATE TABLE episodes (key_md5 varchar(255) primary key, cast_id varchar(255), episode json)');
 	}
 	$db = new SQLite3($dbfile);
+	$db->busyTimeout(5000);
+	$db->exec('PRAGMA journal_mode = wal;');
 	$paths = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 	$tab = $paths[0];
 	$id = null;
@@ -32,7 +35,7 @@
 					$results[] = json_decode($row['episode']);
 				}
 				if(count($results) > 0) {
-					echo json_encode($results, JSON_UNESCAPED_UNICODE);
+					echo json_encode($results, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_SLASHES);
 				}
 			}
 		break;
@@ -40,7 +43,6 @@
 			if($id === null) {
 				$db->exec('DELETE FROM '.$tab);
 				$stmt = $db->prepare('INSERT INTO '.$tab." VALUES(?)");
-				print_r($stmt);
 				$stmt->bindValue(1, $reqJSON, SQLITE3_TEXT);
 				$res = $stmt->execute();
 				print_r($res);
@@ -48,20 +50,15 @@
 		break;
 		case "POST":
 			$req = json_decode($reqJSON, true);
-			print_r($req);
-			$mutex = new ExclusiveLock("Sqlite lock");
-			if( $mutex->lock( ) == FALSE )
-    			error("Locking failed");
 			foreach($req['episodes'] as $row) {
 				$md5key = md5($row['mediaURI']);
 				$db->exec("DELETE FROM ".$tab." WHERE key_md5 = '".$md5key."'");
 				$stmt = $db->prepare('INSERT INTO '.$tab.' (key_md5, cast_id, episode) VALUES(?, ?, ?)');
 				$stmt->bindValue(1, $md5key, SQLITE3_TEXT);
 				$stmt->bindValue(2, $req['cast_id'], SQLITE3_TEXT);
-				$stmt->bindValue(3, json_encode($row, JSON_UNESCAPED_UNICODE), SQLITE3_TEXT);
+				$stmt->bindValue(3, json_encode($row, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES), SQLITE3_TEXT);
 				$stmt->execute();
 			}
-			$mutex->unlock();
 			http_response_code(200);
 		break;
 	}
